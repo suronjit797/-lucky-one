93f3287b21e2a0b0f67ad501b69dee7b100873b0
suronjit797 <suronjit797@gmail.com>
2022-03-26 11:29:21 +0600

    props vs hooks

--- src/components/qna/Qna.js
+++ src/components/qna/Qna.js
@@ -6,8 +6,8 @@ const Qna = ({ qna }) => {
         <div className='pt-5'>
             {
                 qna.map((item, index) => (
-                    <div className="qna_sets py-4" key={item._id}>
-                        <h2 className='text-uppercase'> {index+1}. {item.question} </h2>
+                    <div className="qna_sets py-2" key={item._id}>
+                        <h2> {index+1}. {item.question} </h2>
                         <p className='text-justify'> {item.answer} </p>
                     </div>
                 ))
